75727f260a893c396629b1d6096590633e346eb2
zhanghanbo <557451@qq.com>
2024-08-16 14:59:04 +0800

    fix make errors

--- ops/make.sh
+++ ops/make.sh
@@ -13,22 +13,34 @@
 
 # from https://github.com/pytorch/extension-cpp/issues/71#issuecomment-1778326052
 CUDA_VERSION=$(/usr/local/cuda/bin/nvcc --version | sed -n 's/^.*release \([0-9]\+\.[0-9]\+\).*$/\1/p')
-if [[ ${CUDA_VERSION} == 9.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
-elif [[ ${CUDA_VERSION} == 9.2* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
-elif [[ ${CUDA_VERSION} == 10.* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
-elif [[ ${CUDA_VERSION} == 11.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
-elif [[ ${CUDA_VERSION} == 11.* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-elif [[ ${CUDA_VERSION} == 12.* ]]; then
-    export TORCH_CUDA_ARCH_LIST="5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;8.0;8.6;8.7;8.9;9.0+PTX"
-else
-    echo "unsupported cuda version."
-    exit 1
-fi
+echo "CUDA_VERSION: $CUDA_VERSION"
+
+case "${CUDA_VERSION}" in
+    9.0*)
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
+        ;;
+    9.2*)
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
+        ;;
+    10.*)
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
+        ;;
+    11.0*)
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
+        ;;
+    11.*)
+        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+        ;;
+    12.*)
+        export TORCH_CUDA_ARCH_LIST="5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;8.0;8.6;8.7;8.9;9.0+PTX"
+        ;;
+    *)
+        echo "unsupported cuda version."
+        exit 1
+        ;;
+esac
+
+echo "TORCH_CUDA_ARCH_LIST: $TORCH_CUDA_ARCH_LIST"
 
 python -m pip install git+https://github.com/facebookresearch/detectron2.git
 
